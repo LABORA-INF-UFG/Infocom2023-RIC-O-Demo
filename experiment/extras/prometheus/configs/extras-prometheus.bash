ccfb52e9d388f7b690e7b9eb175f1a299994b5f3
zanattabruno <gkbsa2504@gmail.com>
2022-12-27 15:19:58 +0000

    Added Prometheus configs

---
+++ experiment/extras/prometheus/configs/extras-prometheus.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+helm upgrade --install blackbox-node1 prometheus-community/prometheus-blackbox-exporter -f values-blackbox1.yaml -n ricinfra
+helm upgrade --install blackbox-node2 prometheus-community/prometheus-blackbox-exporter -f values-blackbox2.yaml -n ricinfra
+helm upgrade --install blackbox-node3 prometheus-community/prometheus-blackbox-exporter -f values-blackbox3.yaml -n ricinfra
+helm upgrade --install node-exporter prometheus-community/prometheus-node-exporter  -n ricinfra 
+
