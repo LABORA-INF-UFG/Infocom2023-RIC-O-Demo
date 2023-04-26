938cc13a887299646a77c1fc2d03f9f74422698e
Gustavo <gkbsa2504@gmail.com>
2023-04-26 17:01:35 +0000

    Update E2T and E2S initial deployer

---
+++ experiment/deployer/scripts/deploy_E2.py
@@ -0,0 +1,87 @@
+from kubernetes import client, config
+import os
+import yaml
+import time
+
+E2TERM_VALUES = 'helm-charts/e2term/values.yaml'
+E2SIM_VALUES = 'helm-charts/e2sim-helm/values.yaml'
+
+E2TERM_SCTP_LIST = [ {'name': 'node2', 'IP': '10.43.0.227', 'port': 30002}, {'name': 'node3', 'IP': '10.43.0.228', 'port': 30003}]
+
+
+
+def read_yaml(path):
+    with open(path) as file:
+        values = yaml.safe_load(file) 
+    return values
+
+def write_yaml(data, path_file):
+    with open(path_file, 'w') as file:
+        yaml.dump(data, file)
+
+
+def deploy_e2term():
+    config.load_kube_config()
+
+    api_instance = client.CoreV1Api()
+
+    body = {
+        "metadata": {
+            "labels": {
+                "beta.kubernetes.io/arch":"amd64"
+                }
+        }
+    }
+    node_list = api_instance.list_node()
+    values = read_yaml(E2TERM_VALUES)
+    i = 1 
+    for node in node_list.items:
+        if node.metadata.name != 'node1':
+            values = read_yaml(E2TERM_VALUES)
+            values['nodeSelector']['kubernetes.io/hostname'] = node.metadata.name
+            values['services']['sctp']['nodePort']= 30001+i
+            write_yaml(values, E2TERM_VALUES)
+            if i == len(node_list.items) - 1:
+                os.system('helm upgrade --install opt-e2term-'+node.metadata.name+' helm-charts/e2term/ -n ricplt --wait')
+                i = i+1
+            else:
+                os.system('helm upgrade --install opt-e2term-'+node.metadata.name+' helm-charts/e2term/ -n ricplt')
+                i = i+1
+    time.sleep(30)
+
+def deploy_e2sim():
+    config.load_kube_config()
+
+    api_instance = client.CoreV1Api()
+
+    body = {
+        "metadata": {
+            "labels": {
+                "beta.kubernetes.io/arch":"amd64"
+                }
+        }
+    }
+
+    node_list = api_instance.list_node()
+    values = read_yaml(E2SIM_VALUES)
+    i = 1 
+    for node in node_list.items:
+        if node.metadata.name != 'node1':
+            values = read_yaml(E2SIM_VALUES)
+            values['nodeSelector']['kubernetes.io/hostname'] = node.metadata.name
+            values['service']['nodePort'] = 31001+i
+            values['image']['args']['e2term'] = E2TERM_SCTP_LIST[i-1]['IP']
+            values['image']['args']['port'] = E2TERM_SCTP_LIST[i-1]['port']
+            values['image']['args']['gNB'] = i*100
+            values['image']['args']['gNB'] = i
+            write_yaml(values, E2SIM_VALUES)
+            if i == len(node_list.items) - 1:
+                os.system('helm upgrade --install opt-e2sim-'+node.metadata.name+' helm-charts/e2sim-helm -n ricplt --wait')
+                i = i+1
+            else:
+                os.system('helm upgrade --install opt-e2sim-'+node.metadata.name+' helm-charts/e2sim-helm -n ricplt')
+                i = i+1
+
+if __name__ == '__main__':
+    deploy_e2term()
+    deploy_e2sim()
